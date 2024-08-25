2a9874ebbade5ae97827aa173a40bbd18d2da2e7
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-08-25 15:39:37 -0300

    refactor/auth, refatorando a implementação da autentiação, porque tinha esquecido do access_token.

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,7 +1,8 @@
 import axios, { HttpStatusCode } from "axios";
 import { useNavigate } from "react-router-dom";
 import { reactAppBackendUrl } from "../../env/envoriment";
-
+import React, { useState } from "react";
+import "./Auth.css";
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -11,47 +12,58 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`${reactAppBackendUrl}/register`, {
-        name,
-        email,
-        password,
+      await fetch(`${reactAppBackendUrl}/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
       });
       navigate("/login");
     } catch (error) {
       alert(error);
-      throw new HttpStatusCode(400, "Bad request");
+      throw new Error({ message: error.message });
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
-      <label>
-        Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+    <div className="auth-container">
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Register</button>
+        <p>
+          Already have an account? <a href="/login">Login</a>
+        </p>
+      </form>
+    </div>
   );
 }
 
