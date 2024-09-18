c4b2c70a262e73e9428817f7f48a74f9db8ea70c
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-18 13:58:59 -0300

    feat/footer, correção de algumas paginas, componentes e estilos

--- src/pages/Dashsboard.jsx
+++ src/pages/Dashsboard.jsx
@@ -1,10 +1,15 @@
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFileArchive,
+  faRightLeft,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import TaskManager from "../components/tasks/TaskManager";
 import "./Dashboard.css";
 import Reports from "./Reports";
+import { Button } from "bootstrap";
 
 /**
  * A functional component representing the application's dashboard.
@@ -27,10 +32,19 @@ function Dashboard() {
     navigate("/login");
   };
 
+  const reports = async () => {
+    localStorage.getItem("token");
+    navigate("/reports");
+  };
+
   return (
     <div className="dashboard">
-      <header>
+      <header className="header-dashboard">
         <h1>Dashboard</h1>
+        <button onClick={reports} className="reports-button">
+          <FontAwesomeIcon icon={faFileArchive} />
+          Reports
+        </button>
         <button onClick={logout} className="logout-button">
           <FontAwesomeIcon icon={faSignOutAlt} />
           Logout
